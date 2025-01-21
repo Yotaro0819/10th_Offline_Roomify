abc50d3953cbdadc4f6d3497880a5537b75cbcf5
Daisuke Yamashita <daisuke@DaisukenoMacBook-Air.local>
2025-01-21 14:03:36 +0800

    coupon

--- resources/views/coupon.blade.php
+++ resources/views/coupon.blade.php
@@ -6,34 +6,213 @@
 
 <style>
 
-h1
+@import url('https://fonts.googleapis.com/css2?family=Monoton&display=swap');
+
+html,body
+    {
+        position: relative;
+    }
+
+
+.coupon-title {
+font-family: 'Monoton', sans-serif;
+font-size: 3rem;
+text-align: center;
+color: #dcbf7d;
+text-shadow: 
+    2px 2px 4px rgba(0, 0, 0, 0.3),
+    4px 4px 10px rgba(0, 0, 0, 0.2);
+background: linear-gradient(135deg, #f9d976, #f39c12);
+background-clip: text;
+-webkit-background-clip: text;
+color: transparent;
+position: relative;
+transform: rotateX(10deg) rotateY(10deg);
+}
+
+
+.coupon {
+    border: 2px solid #dcbf7d;
+    display: flex;
+    align-items: stretch;
+    box-shadow: 0px 4px 8px rgba(0, 0, 0, 0.1); 
+    border-radius: 10px; 
+    background-color: white;
+}
+
+.col-3 {
+    border-right: 2px dashed #dcbf7d !important;
+    font-family: 'Monoton';
+    display: flex;
+    align-items: center;
+    justify-content: center;
+
+}
+
+.col-9 {
+    border-left: none !important;
+    display: flex;
+    align-items: center;
+}
+
+.coupon_price
 {
     color: #004aad;
 }
 
-.coupon
+.star {
+    width: 100px;
+    height: 100px;
+    background-color: #dcbf7d;
+    clip-path: polygon(
+        50% 0%, 61% 35%, 98% 35%, 
+        68% 57%, 79% 91%, 50% 70%, 
+        21% 91%, 32% 57%, 2% 35%, 
+        39% 35%
+    );
+    position: absolute;
+    box-shadow: 4px 4px 10px rgba(0, 0, 0, 0.3), -4px -4px 10px rgba(0, 0, 0, 0.1);
+    background: radial-gradient(circle, #f2e0a9, #dcbf7d);
+    transform: rotateX(10deg) rotateY(10deg); 
+    z-index: -1;
+}
+
+.star1
 {
-    border: 2px solid #dcbf7d;
+    width: 200px;
+    height: 200px;
+    top: 150px;
+    left: 300px;
+    transform: rotate(15deg); 
+}
+
+.star2 
+{
+    width: 150px;
+    height: 150px;
+    top: 400px;
+    left: 200px;
+    transform: rotate(30deg); 
+}
+
+.star3
+{
+    width: 50px;
+    height: 50px;
+    top: 200px;
+    left: 100px;
+    transform: rotate(15deg); 
+}
+
+.star4
+{
+    width: 500px;
+    height: 500px;
+    top: 150px;
+    right: 350px;
+    transform: rotate(45deg); 
+}
+
+.star5
+{
+    width: 150px;
+    height: 150px;
+    top: 100px;
+    right: 200px;
+    transform: rotate(45deg); 
+}
+
+.star6
+{
+    width: 100px;
+    height: 100px;
+    top: 450px;
+    right: 100px;
+    transform: rotate(15deg); 
 }
 
 </style>
 
+
 <div class="container">
+
+    <div class="star star1"></div>
+    <div class="star star2"></div>
+    <div class="star star3"></div>
+    <div class="star star4"></div>
+    <div class="star star5"></div>
+    <div class="star star6"></div>
+    
     <div class="row d-flex justify-content-center align-items-center">
         <div class="col-5">
-            <h1 class="text-center">Your Coupon</h1>
-    
+            <h1 class="coupon-title text-center">Your Coupon</h1>
+        
+            <div class="row coupon mb-3">
+                <div class="col-3">
+                    COUPON
+                </div>
+                <div class="col-9">
+                    <div>
+                        <p class="m-0 mt-2 coupon_price">10%OFF</p>
+                        
+                        <p class="m-0 mb-2">Expiration Date: 05/06/2025</p>
+                    </div>
+                </div>
+            </div>
 
-            <div class="coupon">
-                
-                
+            <div class="row coupon mb-3">
+                <div class="col-3">
+                    COUPON
+                </div>
+                <div class="col-9">
+                    <div>
+                        <p class="m-0 mt-2 coupon_price">10%OFF</p>
+                        
+                        <p class="m-0 mb-2">Expiration Date: 05/06/2025</p>
+                    </div>
+                </div>
+            </div>
+
+            <div class="row coupon mb-3">
+                <div class="col-3">
+                    COUPON
+                </div>
+                <div class="col-9">
+                    <div>
+                        <p class="m-0 mt-2 coupon_price">10%OFF</p>
+                        
+                        <p class="m-0 mb-2">Expiration Date: 05/06/2025</p>
+                    </div>
+                </div>
+            </div>
+
+            <div class="row coupon mb-3">
+                <div class="col-3">
+                    COUPON
+                </div>
+                <div class="col-9">
+                    <div>
+                        <p class="m-0 mt-2 coupon_price">10%OFF</p>
+                        
+                        <p class="m-0 mb-2">Expiration Date: 05/06/2025</p>
+                    </div>
+                </div>
+            </div>
+
+            <div class="row coupon mb-3">
+                <div class="col-3">
+                    COUPON
+                </div>
+                <div class="col-9">
+                    <div>
+                        <p class="m-0 mt-2 coupon_price">10%OFF</p>
+                        
+                        <p class="m-0 mb-2">Expiration Date: 05/06/2025</p>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
 </div>
-
-
-
-
 
 @endsection
