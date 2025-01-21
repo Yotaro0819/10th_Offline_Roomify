d5369de0dcc147062176cd557650d4af4a36dec9
鎌田美空 <kamatamisora@kamatamisoranoMacBook-Air.local>
2025-01-21 14:28:19 +0800

    guest/profile/frontend

--- routes/web.php
+++ routes/web.php
@@ -6,3 +6,7 @@ use Illuminate\Support\Facades\Route;
 Auth::routes();
 
 Route::get('/', [App\Http\Controllers\HomeController::class, 'index'])->name('index');
+
+Route::get('/profile',function(){
+    return view('guest_profile');
+});
