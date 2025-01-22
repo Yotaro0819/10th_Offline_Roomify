2f01d23066b52835971bd630c3d24ba7f49e7ef8
Yoku Araki <ay.araki0819@outlook.jp>
2025-01-23 00:32:41 +0800

    create_accommodation_model

--- app/Models/Nationality.php
+++ app/Models/Nationality.php
@@ -2,9 +2,22 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Nationality extends Model
 {
-    //
+    use HasFactory;
+
+    protected $table = 'nationalities';
+
+    protected $fillable = [
+        'name',
+    ];
+
+    public function users()
+    {
+        return $this->hasMany(User::class);
+    }
+
 }
