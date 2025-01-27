43fcc7fe3cf300a5d707168628d935d28f9c625c
鎌田美空 <kamatamisora@kamatamisoranoMacBook-Air.local>
2025-01-27 17:04:48 +0800

    host/res/backend

--- app/Models/Booking.php
+++ app/Models/Booking.php
@@ -10,4 +10,16 @@ class Booking extends Model
     public function user(){
         return $this->belongsTo(User::class);
     }
+
+    public function showBookingStatus($bookingId)
+{
+    $booking = Booking::find($bookingId);
+
+    
+    if (!$booking) {
+        return redirect()->back()->with('error', 'Booking not found.');
+    }
+
+    return view('hostRes', compact('booking'));
+}
 }
