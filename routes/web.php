ebf07198ce8b320306791da501c53f9bd6f876c2
Yoku Araki <ay.araki0819@outlook.jp>
2025-01-27 17:04:47 +0800

    route_changed

--- routes/web.php
+++ routes/web.php
@@ -92,6 +92,8 @@ Route::group(['prefix' => 'host', 'as' => 'host.', 'middleware' => 'host'], func
         return view('hostRes');});
     Route::get('/acmindex', [AccommodationController::class, 'index'])->name('index');
     Route::delete('/{id}/destroy,', [AccommodationController::class, 'destroy'])->name('destroy');
+    Route::get('/accommodation/create', [AccommodationController::class, 'create'])->name('accommodation.create');
+    ROute::post('/accommodation/store', [AccommodationController::class, 'store'])->name('accommodation.store');
 
 });
 
