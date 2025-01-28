5a355300ca7ae8d5bc7b8cd8edd90ffc4594b20d
Yoku Araki <ay.araki0819@outlook.jp>
2025-01-28 17:16:36 +0800

    show_revice/yoku

--- resources/views/accommodation/pictures.blade.php
+++ resources/views/accommodation/pictures.blade.php
@@ -2,41 +2,28 @@
 
 @section('title', 'pictures')
 
+<style>
+    .images {
+        width: 500px;
+        object-fit:cover;
+    }
+</style>
 @section('content')
 
 <div class="container w-75 mx-auto">
     <h2 class="h1 my-5">Photos</h2>
-    <a href="#" class="text-black"><i class="fa-solid fa-angles-left"></i> Back to the accommodation page</a>
+    <a href="{{ route('accommodation.show', $accommodation->id)}}" class="text-black"><i class="fa-solid fa-angles-left"></i> Back to the accommodation page</a>
 
     <div class="row mt-5">
-        <!-- 写真1 -->
 
         @foreach ($accommodation->photos as $photo)
-
-        @endforeach
         <div class="col-6">
-          <img src="{{ asset('asset_Araki/chintai-14-670x448.jpg')}}" alt="Image 1" class="img-fluid rounded-5 mb-3">
-        </div>
-        <!-- 写真2 -->
-        <div class="col-6">
-          <img src="{{ asset('asset_Araki/DSC08197-670x448.jpg')}}" alt="Image 2" class="img-fluid rounded-5 mb-3">
-        </div>
-        <!-- 写真3 -->
-        <div class="col-6">
-          <img src="{{ asset('asset_Araki/DSC08197-670x448.jpg')}}" alt="Image 3" class="img-fluid rounded-5 mb-3">
-        </div>
-        <!-- 写真4 -->
-        <div class="col-6">
-          <img src="{{ asset('asset_Araki/DSC08197-670x448.jpg')}}" alt="Image 4" class="img-fluid rounded-5 mb-3">
-        </div>
-
-        <div class="col-6">
-            <img src="{{ asset('asset_Araki/DSC08197-670x448.jpg')}}" alt="Image 5" class="img-fluid rounded-5 mb-3">
+            <img src="{{ asset('storage/'. $photo->image)}}" alt="Images" class="img-fluid rounded-5 mb-3 images" style="height:400px;">
           </div>
+        @endforeach
+
       </div>
     </div>
 </div>
-
-
 
 @endsection
