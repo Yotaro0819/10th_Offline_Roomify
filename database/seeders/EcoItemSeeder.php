45d372a52036e158273096647e4522d7412af7bb
Yoku Araki <ay.araki0819@outlook.jp>
2025-03-06 17:30:50 +0800

    ecoitem

--- database/seeders/EcoItemSeeder.php
+++ database/seeders/EcoItemSeeder.php
@@ -2,39 +2,30 @@
 
 namespace Database\Seeders;
 
-use App\Models\Accommodation;
-use App\Models\EcoItem;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
-class EcoItemAccommodationSeeder extends Seeder
+class EcoitemSeeder extends Seeder
 {
     /**
      * Run the database seeds.
      */
-    private $eco;
-    public function __construct(EcoItem $ecoitem)
-    {
-        $this->eco = $ecoitem;
-    }
     public function run(): void
     {
-        $accommodations = Accommodation::all();
-        $eco_items = $this->eco->all();
+        $ecoitems = [
+            ['ecoitem_name' => 'No amenities', 'point' => 5],
+            ['ecoitem_name' => 'Dishes available', 'point' => 5],
+            ['ecoitem_name' => 'Strict weste separation', 'point' => 5],
+            ['ecoitem_name' => 'Bicycle rental', 'point' => 10],
+            ['ecoitem_name' => 'Local Ingredients provided', 'point' => 10],
+            ['ecoitem_name' => 'Carbon offset', 'point' => 10],
+            ['ecoitem_name' => 'Solar panels', 'point' => 15],
+            ['ecoitem_name' => 'LED lighting', 'point' => 15],
+            ['ecoitem_name' => 'Water-saving', 'point' => 15],
+            ['ecoitem_name' => 'Activities', 'point' => 15]
+        ];
 
-        $eco_ids = [];
-
-        foreach($eco_items as $eco){
-            $eco_ids[] = $eco->id;
-        }
-
-
-
-        foreach ($accommodations as $accommodation) {
-
-            $randomEcoitems = array_rand(array_flip($eco_ids), rand(1, count($eco_ids)));
-
-            $accommodation->ecoitems()->sync($randomEcoitems);
-        }
+        DB::table('ecoitems')->insert($ecoitems);
     }
 }
